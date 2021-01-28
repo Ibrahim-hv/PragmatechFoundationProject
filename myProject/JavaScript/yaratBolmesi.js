26982bd0f4f04ed1b4d5b6eeda0b2b26faffee8d
Ibrahim Huseyn <76060139+Ibrahim-hv@users.noreply.github.com>
2021-01-29 02:24:58 +0400

    öz layihəmi davam edirəm

--- myProject/JavaScript/yaratBolmesi.js
+++ myProject/JavaScript/yaratBolmesi.js
@@ -76,3 +76,26 @@ function closeAllSelect(elmnt) {
 /*if the user clicks anywhere outside the select box,
 then close all select boxes:*/
 document.addEventListener("click", closeAllSelect);
+
+
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
