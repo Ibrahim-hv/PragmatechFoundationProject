ba99b1d70ad970b51e769365da650c5d26f0daf3
Ibrahim Huseyn <76060139+Ibrahim-hv@users.noreply.github.com>
2021-02-19 19:37:08 +0400

    No Complated

---
+++ myProject/Python/run.py
@@ -0,0 +1,24 @@
+def Foo():
+    a=input('1ci reqemi daxil edin:')
+    b=input('2ci reqemi daxil edin:')
+    c=int(a)+int(b)
+    print(c)
+    print(c/2)
+# Foo()
+
+a='admin'
+b='123456'
+
+def Sorgu():
+    us=input('Username daxil edin:')
+    pas=input('Pasvord daxil edin:')
+    if us==a and pas==b:
+        print('Dogrudur')
+    else:
+        print('Yanlisdir')
+
+# Sorgu()
+
+
+
+
